f007298426e3b4183a988d0821f362c3d45d0d95
Giulio <giupersu@yahoo.it>
2018-07-09 11:32:56 +0200

    Create the Service Provider

---
+++ InspireServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+namespace Tetravalence\Inspire;
+
+use Illuminate\Support\ServiceProvider;
+
+class InspireServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
