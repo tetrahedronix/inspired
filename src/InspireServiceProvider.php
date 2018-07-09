e61b18c889503b00f51fba4608759ba08b7a682d
Giulio <giupersu@yahoo.it>
2018-07-09 18:03:44 +0200

    Move bootstraping services to their own methods

--- src/InspireServiceProvider.php
+++ src/InspireServiceProvider.php
@@ -24,33 +24,72 @@ class InspireServiceProvider extends ServiceProvider
         /* Typically, you will need to publish your package's configuration file
          * to the application's own config directory.
          */
-        $this->publishes([
-            dirname(dirname(__FILE__)).
-                '/config/inspire.php' => config_path('inspire.php'),
-            ], 'inspire-config');
+        $this->bootAssetPublishing();
 
         /* If your package contains routes, you may load them using the
          * loadRoutesFrom method.
          */
-         Route::group([
-             'namespace' => 'Tetravalence\Inspire\Http\Controllers',
-         ], function () {
-            $this->loadRoutesFrom(dirname(dirname(__FILE__)).
-                '/routes/blog.php');
-        });
+        $this->bootRoutes();
 
         /* If your package conains database migrations, you may use the
          * loadMigrationsFrom method to inform Laravel how to load them.
          */
-        $this->loadMigrationsFrom(dirname(dirname(__FILE__)).
-            '/database/migrations');
+        $this->bootMigrations();
 
         /* To register the package's views with Laravel, you need to use the
          * service provider's LoadViewsFrom method.
          */
-         $this->loadViewsFrom(dirname(dirname(__FILE__)).
-            '/resources/views', 'inspire');
+        $this->bootResources();
+    }
 
+    /**
+     * Define the asset publishing configuration.
+     *
+     * @return void
+     */
+    protected function bootAssetPublishing()
+    {
+        $this->publishes([
+            dirname(dirname(__FILE__)).
+                '/config/inspire.php' => config_path('inspire.php'),
+            ], 'inspire-config');
+    }
+
+    /**
+     * Register the Inspire migrations.
+     *
+     * @return void
+     */
+    protected function bootMigrations()
+    {
+        $this->loadMigrationsFrom(dirname(dirname(__FILE__)).
+            '/database/migrations');
+    }
+
+    /**
+     * Register the Inspire resources.
+     *
+     * @return void
+     */
+    protected function bootResources()
+    {
+        $this->loadViewsFrom(dirname(dirname(__FILE__)).
+           '/resources/views', 'inspire');
+    }
+
+    /**
+     * Register the Inspire routes.
+     *
+     * @return void
+     */
+    protected function bootRoutes()
+    {
+        Route::group([
+            'namespace' => 'Tetravalence\Inspire\Http\Controllers',
+        ], function () {
+           $this->loadRoutesFrom(dirname(dirname(__FILE__)).
+               '/routes/blog.php');
+       });
     }
 
     /**
