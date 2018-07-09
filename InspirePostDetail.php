399326f3e2283afb760e8048ae85d11d9f05825f
Giulio <giupersu@yahoo.it>
2018-07-09 11:49:33 +0200

    Define the primary key for post details

--- InspirePostDetail.php
+++ InspirePostDetail.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class InspirePostDetail extends Model
 {
-    //
+    /*
+    |--------------------------------------------------------------------------
+    | Primary Key
+    |--------------------------------------------------------------------------
+    |
+    | Eloquent will also assume that each table has a primary key column
+    | named id. You may define a protected $primaryKey property to override
+    | this convention.
+    */
+    protected $primaryKey = 'post_details_id';
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
 }
