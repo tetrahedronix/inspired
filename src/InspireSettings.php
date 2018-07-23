cd23e84b85aef3ea9d2d20189ac504d48e9a2363
Giulio <giupersu@yahoo.it>
2018-07-23 13:33:16 +0200

    Add getter method

--- src/InspireSettings.php
+++ src/InspireSettings.php
@@ -12,4 +12,11 @@ class InspireSettings extends Model
     // Specify a custom table by defining this property.
     protected $table = 'settings';
 
+    public function getValue($key)
+    {
+        $settings_array = $this->where('settings_key', $key)->
+            firstOrFail()->toArray();
+
+        return $settings_array['settings_value'];
+    }
 }
