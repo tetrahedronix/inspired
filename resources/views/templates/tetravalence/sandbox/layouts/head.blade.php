a44280b658498d4bc7686465a7b29e50606e37e4
Giulio <giupersu@yahoo.it>
2018-08-17 17:27:47 +0200

    Add Bootstrap stylesheet link

--- resources/views/templates/tetravalence/sandbox/layouts/head.blade.php
+++ resources/views/templates/tetravalence/sandbox/layouts/head.blade.php
@@ -7,5 +7,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <!-- Styles -->
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <!-- Custom styles for this template -->
         <link rel="stylesheet" href="{{ asset('/vendor/tetravalence/assets/css/master.css') }}">
