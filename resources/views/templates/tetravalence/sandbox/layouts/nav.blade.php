0e5fe784bc4e399560b018422971169c3ee8c156
Giulio <giupersu@yahoo.it>
2018-08-18 21:18:26 +0200

    Add Bootstrap's responsive navigation header

--- resources/views/templates/tetravalence/sandbox/layouts/nav.blade.php
+++ resources/views/templates/tetravalence/sandbox/layouts/nav.blade.php
@@ -1,17 +1,19 @@
 <nav class="navbar navbar-expand-lg navbar-light fixed-top">
-  <div class="links">
-  <a href="https://laravel.com/docs">Documentation</a>
-  <a href="https://laracasts.com">Laracasts</a>
-  <a href="https://laravel-news.com">News</a>
-  <a href="https://forge.laravel.com">Forge</a>
-  <a href="https://github.com/laravel/laravel">GitHub</a>
-    @if (Route::has('login'))
-      @auth
-        <a href="{{ url('/home') }}">Home</a>
-      @else
-        <a href="{{ route('login') }}">Login</a>
-        <a href="{{ route('register') }}">Register</a>
-      @endauth
-    @endif
-  </div>
+
+    <ul class="navbar-nav mx-auto text-center">
+      <li class="links nav-item"><a class="nav-link" href="https://laravel.com/docs">Documentation</a></li>
+      <li class="links nav-item"><a class="nav-link" href="https://laracasts.com">Laracasts</a></li>
+      <li class="links nav-item"><a class="nav-link" href="https://laravel-news.com">News</a></li>
+      <li class="links nav-item"><a class="nav-link" href="https://forge.laravel.com">Forge</a></li>
+      <li class="links nav-item"><a class="nav-link" href="https://github.com/laravel/laravel">GitHub</a></li>
+        @if (Route::has('login'))
+          @auth
+            <li class="links nav-item active"><a class="nav-link" href="{{ url('/home') }}">Home</a></li>
+          @else
+            <li class="links nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
+            <li class="links nav-item"><a class="nav-link" href="{{ route('register') }}">Register</a></li>
+          @endauth
+        @endif
+    </ul>
+
 </nav>
