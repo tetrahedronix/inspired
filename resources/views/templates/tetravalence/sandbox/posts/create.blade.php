af8acdaf3b7c82d21226d293ab7b88dbaf4566bd
Giulio <giupersu@yahoo.it>
2018-08-21 21:16:16 +0200

    Add code to submit the post content

--- resources/views/templates/tetravalence/sandbox/posts/create.blade.php
+++ resources/views/templates/tetravalence/sandbox/posts/create.blade.php
@@ -2,21 +2,22 @@
 
 @section('posts')
   <h1>Create a  post</h1>
-  <form>
+
+  <form method="post" action="/posts">
+
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <label for="title">Title</label>
+    <input type="text" class="form-control" id="title" name="post_title" aria-describedby="Post title" placeholder="Enter title here">
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
+
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="text-editor">Post</label>
+    <textarea class="form-control" name="text-editor" rows="8" cols="80"></textarea>
   </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+
+  <button type="submit" class="btn btn-primary">Publish</button>
+
 </form>
 @endsection
 
