7ac96e466f16df8e48bfabbefb8600d17ad5c373
Giulio <giupersu@yahoo.it>
2018-08-21 22:10:38 +0200

    Declare a resource route to specify a subset of actions

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,26 @@
 <?php
-
-Route::get('/', 'InspiredPostController@index');
-
-Route::get('/post/id/{post}', 'InspiredPostController@show')
-    ->where('post', '[0-9]+');
-
-Route::get('/post/create', 'InspiredPostController@create');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+/*
+ * This single route declaration creates multiple routes to handle a variety
+ * of actions on the route.
+ */
+Route::resources([
+    'posts' => 'InspiredPostController']
+)->only(['index', 'show']);
 
 Route::middleware('web', 'auth', 'dashboard')->group(function () {
     Route::redirect('/home', '/dashboard');
     Route::get('/dashboard', 'InspiredDashboardController@index');
+    Route::get('/dashboard/create', 'InspiredDashboardController@create');
     Route::get('/dashboard/template', 'InspiredDashboardController@template')->
       name('template');
 });
