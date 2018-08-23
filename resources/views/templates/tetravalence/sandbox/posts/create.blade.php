6c340abca8903c7ca35697a2f2c7cc74e56b7f5a
Giulio <giupersu@yahoo.it>
2018-08-23 19:21:12 +0200

    Usa HTML5 browser validation

--- resources/views/templates/tetravalence/sandbox/posts/create.blade.php
+++ resources/views/templates/tetravalence/sandbox/posts/create.blade.php
@@ -9,13 +9,13 @@
 
   <div class="form-group">
     <label for="title">Title</label>
-    <input type="text" class="form-control" id="title" name="post_title" aria-describedby="Post title" placeholder="Enter title here">
+    <input type="text" class="form-control" id="title" name="post_title" aria-describedby="Post title" placeholder="Enter title here" required>
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
 
   <div class="form-group">
     <label for="text-editor">Post</label>
-    <textarea class="form-control" name="post_body" rows="8" cols="80"></textarea>
+    <textarea class="form-control" name="post_body" rows="8" cols="80" required></textarea>
   </div>
 
   <button type="submit" class="btn btn-primary">Publish</button>
