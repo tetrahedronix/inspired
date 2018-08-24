c15199f28e1a1052eeef7c7aac30acc7f0ed3025
Giulio <giupersu@yahoo.it>
2018-08-24 19:41:09 +0200

    Write the preliminary code for validation logic

--- src/Http/Controllers/InspiredDashboardPostController.php
+++ src/Http/Controllers/InspiredDashboardPostController.php
@@ -43,7 +43,82 @@ class InspiredDashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        /*
+        |-----------------------------------------------------------------------
+        | Validation
+        |-----------------------------------------------------------------------
+        | accepted The field under validation must be yes, on, 1, or true.
+        |          This is useful for validating "Terms of Service" acceptance.
+        | after:date The field under validation must be a value after a given
+        |            date.
+        | after_or_equal:date The field under validation must be a value after
+        |                or equal to the given date.
+        | alpha_dash The field under validation may have alpha-numeric
+        |            characters, as well as dashes and underscores.
+        | in The field under validation must be included in the given list of
+        |    values.
+        | min:value The field under validation must have a minimum value.
+        | max:value The field under validation must be less than or equal to a
+        |           maximum value.
+        | nullable The field under validation may be null.
+        | numeric The field under validation must be numeric.
+        | required The field under validation must be present in the input data
+        |          and not empty.
+        | string The field under validation must be a string.
+        */
+        $this->validate(request(), [
+            'post_title' => 'required|min:1|max:512',
+            'post_body' => 'required',
+            'post_discuss_status' => 'required|in:open,closed',
+            'post_excerpt' => 'nullable',
+            'post_protected' => 'required|string|in:yes,no',
+            'post_parent' => 'required|numeric',
+            'post_slug' => 'required|alpha_dash',
+            'post_metakeys' => 'present|nullable',
+        ]);
+
+        // Create a new Post using the request data
+        $post = new Post;
+
+        // Fill the table with the request data
+        $post->post_uid = $request->user()->id;
+        $post->post_title = request('post_title');
+
+        // Save it to the database
+        $post->save();
+
+        // Create a new PostDetail using the request data
+        $post_detail = new PostDetail;
+
+        // Fill the table with the request data
+        $post_detail->post_id = Post::where('post_uid', $post->post_uid)->
+            orderBy('id', 'desc')->first()->id;
+        $post_detail->post_body = request('post_body');
+        $post_detail->post_discuss_status = 'open';
+
+        if (empty(request('post_excerpt'))) {
+            $post_detail->post_excerpt = '';
+        } else {
+            $post_detail->post_excerpt = request('post_excerpt');
+        }
+
+        $post_detail->post_protected = request('post_protected');
+        $post->post_uid = $request->user()->id;
+        // to validate further
+        $post_detail->post_parent = request('post_parent');
+        $post_detail->post_slug = request('post_slug');
+
+        if ( empty(request('post_metakeys'))) {
+            $post_detail->post_metakeys = '';
+        } else {
+            $post_detail->post_metakeys = request('post_metakeys');
+        }
+
+        // Save it to the database
+        $post_detail->save();
+
+        // Redirect
+        return redirect('/dashboard/posts');
     }
 
     /**
