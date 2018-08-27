29cba0de573f1dfacbed9612a06015ff42fa4a55
Giulio <giupersu@yahoo.it>
2018-08-27 23:17:34 +0200

    Use `empty()` instead of `isEmpty`

--- resources/views/templates/tetravalence/sandbox/posts/single.blade.php
+++ resources/views/templates/tetravalence/sandbox/posts/single.blade.php
@@ -3,14 +3,13 @@
   <div class="blog-post">
 
     <h2 class="blog-post-title"><a href="posts/{{ $post->id }}">{{ $post->post_title }}</a></h2>
-
+    @if (! empty( $post_details))
     <p class="blog-post-meta">
 
       {{ $post_details[$post->id-1]->post_date->toFormattedDateString() }} <a href="#">Giulio</a>
 
     </p>
 
-    @if (! $post_details->isEmpty())
       {{ $post_details[$post->id-1]->post_body }}
     @endif
   </div>
