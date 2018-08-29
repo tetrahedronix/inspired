1ab793d7be7ed7d8fbff9b6f1a5aba5bd47ea93e
Giulio <giupersu@yahoo.it>
2018-08-29 19:49:58 +0200

    Define a one-to-many relationship between `Post` and `Comment` models

--- src/InspiredPost.php
+++ src/InspiredPost.php
@@ -10,8 +10,15 @@ class InspiredPost extends InspiredModel
 
     protected $table = 'posts';
 
+    public function comments()
+    {
+        return $this->hasMany('Tetravalence\Inspired\InspiredComment',
+            'comment_post_id');
+    }
+
     public function details()
     {
-        return $this->hasOne('Tetravalence\Inspired\InspiredPostDetail', 'post_id', 'id');
+        return $this->hasOne('Tetravalence\Inspired\InspiredPostDetail',
+            'post_id', 'id');
     }
 }
