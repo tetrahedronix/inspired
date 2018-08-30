e04f9ed7dd8fa93fc739adaebeb96c0497e815cd
Giulio <giupersu@yahoo.it>
2018-08-30 17:39:21 +0200

    Remove e-mail help from form group

--- resources/views/dashboard/create.blade.php
+++ resources/views/dashboard/create.blade.php
@@ -10,7 +10,6 @@
     <div class="form-group">
       <label for="post_title">Title</label>
       <input type="text" class="form-control" id="title" name="post_title" aria-describedby="Post title" placeholder="Enter title here" required>
-      <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
 
     <div class="form-group">
